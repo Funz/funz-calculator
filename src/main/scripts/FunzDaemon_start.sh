96342b7faa586d87d98f64fbfc47d70745048c3a
yannrichet <richetyann@gmail.com>
2021-09-06 12:15:27 +0200

    fix slf4j dep

--- src/main/scripts/FunzDaemon_start.sh
+++ src/main/scripts/FunzDaemon_start.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 MAIN=org.funz.calculator.Calculator
 
-LIB=`find -L lib -name "funz-core-*.jar"`:`find -L lib -name "funz-calculator-*.jar"`:`find -L lib -name "commons-io-2.4.jar"`:`find -L lib -name "commons-exec-*.jar"`:`find -L lib -name "commons-lang-*.jar"`:`find -L lib -name "ftpserver-core-*.jar"`:`find -L lib -name "ftplet-api-*.jar"`:`find -L lib -name "mina-core-*.jar"`:`find -L lib -name "sigar-*.jar"`:`find -L lib -name "slf4j-api-*.jar"`:`find -L lib -name "slf4j-log4j12-*.jar"`
+LIB=`find -L lib -name "funz-core-*.jar"`:`find -L lib -name "funz-calculator-*.jar"`:`find -L lib -name "commons-io-2.4.jar"`:`find -L lib -name "commons-exec-*.jar"`:`find -L lib -name "commons-lang-*.jar"`:`find -L lib -name "ftpserver-core-*.jar"`:`find -L lib -name "ftplet-api-*.jar"`:`find -L lib -name "mina-core-*.jar"`:`find -L lib -name "sigar-*.jar"`:`find -L lib -name "slf4j-api-*.jar"`:`find -L lib -name "slf4j-nop-*.jar"`
 
 CALCULATOR=file:calculator.xml
 
